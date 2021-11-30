9599b461513a060b9402aabf2b9e15ff8db50b55
Martyna Stanislawska <martynastan@gmail.com>
2021-11-30 19:46:58 +0000

    get by id route working for all classes

--- src/main/java/codeclan/files_and_folders/Files_and_folders/controllers/UserController.java
+++ src/main/java/codeclan/files_and_folders/Files_and_folders/controllers/UserController.java
@@ -22,4 +22,10 @@ public class UserController {
         return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
 
     }
+
+    @GetMapping(value = "/users/{id}")
+    public ResponseEntity getUser(@PathVariable Long id) {
+        return new ResponseEntity(userRepository.findById(id), HttpStatus.OK);
+
+    }
 }
